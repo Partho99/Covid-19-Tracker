bcd85558911ab942e5a0709f83fad2313e616b04
Partho Das <partho.swe@gmail.com>
2021-05-13 00:55:31 +0600

    using fetch api to fetch rest api data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import {FormControl, MenuItem, Select} from "@material-ui/core";
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [countries, setCountries] = useState([]);
+
+    useEffect(() => {
+        const getCountriesData = async () => {
+            await fetch('https://disease.sh/v3/covid-19/countries')
+                .then(response => {
+                    if (response.ok) {
+                        return response.json()
+                    }
+                    throw response;
+                })
+                .then((data) => {
+                    const countryList = data.map(country => ({
+                        name: country.country,
+                        value: country.countryInfo.iso2,
+                    }))
+                    setCountries(countryList);
+                })
+        }
+
+        getCountriesData()
+            .then(r => r)
+            .catch(e => console.log(e))
+    }, []);
+
+    return (
+        <div className="App">
+            <div className={'app__header'}>
+                <h1>COVID-19-TRACKER</h1>
+                <FormControl className={'app__dropdown'}>
+                    <Select
+                        variant={'outlined'}
+                        value={"abc"}
+                    >
+                        {
+                            countries.map(country => (
+                                <MenuItem value={country.value}>{country.name}</MenuItem>
+                            ))
+                        }
+                    </Select>
+                </FormControl>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
